93198026fba8e685551db6c133aec29175586e5f
starcroce-PC <starcroce@yahoo.com>
2015-04-15 00:30:49 -0700

    add insertion sort, todo: time complexity analysis

---
+++ insertion_sort.py
@@ -0,0 +1,27 @@
+import random
+
+
+def insertion_sort(arr):
+    if len(arr) <= 1:
+        return arr
+
+    for i in range(1, len(arr)):
+        tmp, j = arr[i], i - 1
+        while j >= 0 and tmp < arr[j]:
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = tmp
+
+    return arr
+
+
+def main():
+    arr = [x for x in range(10)]
+    random.shuffle(arr)
+    print arr
+
+    arr = insertion_sort(arr)
+    print arr
+
+if __name__ == '__main__':
+    main()
