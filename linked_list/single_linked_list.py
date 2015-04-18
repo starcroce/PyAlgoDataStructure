4a9bddd865d1fc21d4a0f3d10a4b0badf06320e3
starcroce-PC <starcroce@yahoo.com>
2015-04-18 13:18:37 -0700

    add linked list circle operation

--- linked_list/single_linked_list.py
+++ linked_list/single_linked_list.py
@@ -63,24 +63,63 @@ def get_nth_to_last_node(head, n):
     return slow
 
 
+def is_circle(head):
+    fast, slow = head, head
+    while True:
+        fast = fast.next.next
+        slow = slow.next
+
+        if fast is None or fast.next is None:
+            return False
+        if fast is slow:
+            return True
+
+
+def get_circle_start(head):
+    fast, slow = head, head
+    while True:
+        fast = fast.next.next
+        slow = slow.next
+
+        if fast is slow:
+            break
+    fast = head
+    while fast != slow:
+        fast = fast.next
+        slow = slow.next
+    return fast
+
+
 def main():
+    # create a new linked list
     list = LinkedList(0)
     list_tail = get_tail(list.head)
     for i in range(1, 10):
         list.insert_after(list_tail, i)
         list_tail = list_tail.next
-
     print_linked_list(list.head)
 
+    print is_circle(list.head)
+
+    # find 5th to the end node and remove it
+    _5th_to_end = get_nth_to_last_node(list.head, 5)
+    print _5th_to_end.val
+    list.remove_after(_5th_to_end)
+    print_linked_list(list.head)
+
+    # reverse linked list
     new_head = reverse_linked_list(list.head)
     list.head = new_head
     print_linked_list(list.head)
 
-    _5th_to_end = get_nth_to_last_node(new_head, 5)
-    print _5th_to_end.val
+    # create an linked list circle
+    new_tail = get_tail(list.head)
+    _5th_to_end = get_nth_to_last_node(list.head, 5)
+    new_tail.next = _5th_to_end
 
-    list.remove_after(_5th_to_end)
-    print_linked_list(list.head)
+    print is_circle(list.head)
+    circle_start = get_circle_start(list.head)
+    print circle_start.val, _5th_to_end.val
 
 
 if __name__ == '__main__':
