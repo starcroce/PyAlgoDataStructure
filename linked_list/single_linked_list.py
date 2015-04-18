ccf19ad20a44265124809f8743157677a49467e9
starcroce-PC <starcroce@yahoo.com>
2015-04-18 14:26:57 -0700

    change interface

--- linked_list/single_linked_list.py
+++ linked_list/single_linked_list.py
@@ -1,21 +1,21 @@
 import MySingleLinkedList
 
 
-def reverse_linked_list(head):
-    curr, prev = head, None
+def reverse_linked_list(list):
+    curr, prev = list.head, None
     while curr:
         next_node = curr.next
         curr.next = prev
         prev = curr
         curr = next_node
     head = prev
-    return head
+    list.head = head
 
 
-def get_nth_to_last_node(head, n):
+def get_nth_to_last_node(list, n):
     if n <= 0:
         return None
-    fast, slow = head, head
+    fast, slow = list.head, list.head
     for _ in range(n):
         fast = fast.next
         if fast is None:
@@ -25,8 +25,8 @@ def get_nth_to_last_node(head, n):
     return slow
 
 
-def is_circle(head):
-    fast, slow = head, head
+def is_circle(list):
+    fast, slow = list.head, list.head
     while True:
         fast = fast.next.next
         slow = slow.next
@@ -36,14 +36,14 @@ def is_circle(head):
             return True
 
 
-def get_circle_start(head):
-    fast, slow = head, head
+def get_circle_start(list):
+    fast, slow = list.head, list.head
     while True:
         fast = fast.next.next
         slow = slow.next
         if fast is slow:
             break
-    fast = head
+    fast = list.head
     while fast != slow:
         fast = fast.next
         slow = slow.next
@@ -59,26 +59,25 @@ def main():
         list_tail = list_tail.next
     list.print_list()
 
-    print is_circle(list.head)
+    print is_circle(list)
 
     # find 5th to the end node and remove it
-    _5th_to_end = get_nth_to_last_node(list.head, 5)
+    _5th_to_end = get_nth_to_last_node(list, 5)
     print _5th_to_end.val
     list.remove_after(_5th_to_end)
     list.print_list()
 
     # reverse linked list
-    new_head = reverse_linked_list(list.head)
-    list.head = new_head
+    reverse_linked_list(list)
     list.print_list()
 
     # create an linked list circle
     new_tail = list.get_tail()
-    _5th_to_end = get_nth_to_last_node(list.head, 5)
+    _5th_to_end = get_nth_to_last_node(list, 5)
     new_tail.next = _5th_to_end
 
-    print is_circle(list.head)
-    circle_start = get_circle_start(list.head)
+    print is_circle(list)
+    circle_start = get_circle_start(list)
     print circle_start.val, _5th_to_end.val
 
 
