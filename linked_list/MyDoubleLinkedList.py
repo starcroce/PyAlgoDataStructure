4b62cd8440d1b26f209f5bb7547e0dcbe314c3a6
yisha <yisha@securly.com>
2015-04-25 22:41:33 -0700

    fix inserting node issue, tested inserting before / after, removing node method

--- linked_list/MyDoubleLinkedList.py
+++ linked_list/MyDoubleLinkedList.py
@@ -12,28 +12,31 @@ class LinkedList:
         self.head = ListNode(val)
         self.tail = self.head
 
-    def insert(self, prev_node, next_node, val):
-        new_node = ListNode(val)
-        prev_node.next = new_node
-        next_node.prev = new_node
+    def insert(self, prev_node, new_node, next_node):
         new_node.prev = prev_node
+        prev_node.next = new_node
         new_node.next = next_node
+        next_node.prev = new_node
 
     def insert_after(self, node, val):
+        new_node = ListNode(val)
         next_node = node.next
         if next_node is None:
-            node.next = ListNode(val)
-            self.tail = node.next
+            node.next = new_node
+            new_node.prev = node
+            self.tail = new_node
         else:
-            self.insert(node, next_node, val)
+            self.insert(node, new_node, next_node)
 
     def insert_before(self, node, val):
+        new_node = ListNode(val)
         prev_node = node.prev
         if prev_node is None:
-            node.prev = ListNode(val)
-            self.tail = node.prev
+            node.prev = new_node
+            new_node.next = node
+            self.head = new_node
         else:
-            self.insert(prev_node, node, val)
+            self.insert(prev_node, new_node, node)
 
     def remove(self, node):
         prev_node, next_node = node.prev, node.next
--- linked_list/double_linked_list.py
+++ linked_list/double_linked_list.py
@@ -13,18 +13,18 @@ def main():
     for i in range(1, 10):
         double_list.insert_after(double_list.tail, i)
 
+    for i in range(1, 10):
+        double_list.insert_before(double_list.head, i)
+
     double_list.print_list()
 
     _3rd_node = find_nth_node(double_list, 3)
-    print _3rd_node.val, _3rd_node.prev, _3rd_node.next.val
 
     double_list.insert_after(_3rd_node, 100)
     double_list.print_list()
 
-    print _3rd_node.val, _3rd_node.prev#, _3rd_node.next.val
-
-    # double_list.remove(_3rd_node)
-    # double_list.print_list()
+    double_list.remove(_3rd_node)
+    double_list.print_list()
 
 if __name__ == '__main__':
     main()
