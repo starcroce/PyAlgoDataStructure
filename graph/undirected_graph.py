e94a1a086ef5649fc320d24f5a3cb46977326252
starcroce-PC <starcroce@yahoo.com>
2015-05-06 00:47:13 -0700

    implement dijkstra shortest path algorightm

--- graph/undirected_graph.py
+++ graph/undirected_graph.py
@@ -1,4 +1,6 @@
 from collections import deque
+import heapq
+import sys
 
 import  MyUndirectedGraph
 
@@ -30,6 +32,32 @@ def DFS_traversal_helper(start_vertex, res, visited):
             DFS_traversal_helper(adj_vertex, res, visited)
 
 
+def dijkstra_shortest_path(graph, start_id, end_id):
+    distance, prev_vertex, unvisited_queue = {}, {}, []
+    distance[start_id], prev_vertex[start_id] = 0, None
+
+    for v_id in graph.all_vertex:
+        if v_id != start_id:
+            distance[v_id] = sys.maxint
+            prev_vertex[v_id] = None
+        unvisited_queue.append((distance[v_id], v_id))
+
+    while len(unvisited_queue) > 0:
+        heapq.heapify(unvisited_queue)
+        curr_vertex = graph.all_vertex[heapq.heappop(unvisited_queue)[1]]
+        for adj_vertex in curr_vertex.adj_list:
+            alt_dist = distance[curr_vertex.id] + curr_vertex.adj_list[adj_vertex]
+            if alt_dist < distance[adj_vertex.id]:
+                distance[adj_vertex.id] = alt_dist
+                prev_vertex[adj_vertex.id] = curr_vertex.id
+
+    res, dst_id = [], end_id
+    while dst_id:
+        res.append(dst_id)
+        dst_id = prev_vertex[dst_id]
+    return res[::-1]
+
+
 def main():
     """
           b---d
@@ -38,7 +66,7 @@ def main():
          \|/  |/
           c---e
         ab = 4, ac = 2, bc = 1,
-        bd = 4, cd = 8, cd = 10,
+        bd = 4, cd = 8, ce = 10,
         de = 2, df = 6, ef = 3
     """
     my_graph = MyUndirectedGraph.Graph()
@@ -58,10 +86,14 @@ def main():
     my_graph.add_edge('e', 'f', 3)
 
     bfs_traversal_res = BFS_traversal(my_graph.all_vertex['a'])
-    print bfs_traversal_res
+    print 'BFS traversal res:', bfs_traversal_res
 
     dfs_traversal_res = BFS_traversal(my_graph.all_vertex['a'])
-    print dfs_traversal_res
+    print 'DFS traversal res:',dfs_traversal_res
+
+    for id in range(ord('b'), ord('g')):
+        shortest_path = dijkstra_shortest_path(my_graph, 'a', chr(id))
+        print 'shortest path from a to ' + chr(id) + ':', shortest_path
 
 
 if __name__ == '__main__':
