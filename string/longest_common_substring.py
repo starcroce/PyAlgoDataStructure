f1f02f096eb9c3d005823b672d022fec8afdee73
starcroce-PC <starcroce@yahoo.com>
2015-06-03 19:50:13 -0700

    add the solution of suffix array for longest common substring

--- string/longest_common_substring.py
+++ string/longest_common_substring.py
@@ -14,6 +14,7 @@ def longest_common_substring_dp(s1, s2):
                 else:
                     lcs[i][j] = lcs[i-1][j-1] + 1
                 max_len = max(max_len, lcs[i][j])
+
                 if lcs[i][j] >= max_len:
                     max_len = lcs[i][j]
                     res_substr.add(s1[i-max_len+1: i+1])
@@ -24,33 +25,49 @@ def longest_common_substring_dp(s1, s2):
     return max_len, res_substr
 
 
-# def longest_common_substring_suffix_arr(s1, s2):
-#     new_str = s1 + '#' + s2
-#     suffix_arr = ['' for _ in range(len(new_str))]
-#     for i in range(len(new_str)):
-#         suffix_arr[i] = new_str[i:]
-#     suffix_arr.sort()
-#     res = 0
-#     for i in range(len(new_str) - 1):
-#         res = max(res, compare_suffix_str(suffix_arr[i], suffix_arr[i+1]))
-#     return res
-#
-#
-# def compare_suffix_str(s1, s2):
-#     res, i = 0, 0
-#     while s1[i] == s2[i] and s1[i+1] == s2[i+1]:
-#         res += 1
-#         if s1[i+2] == '#' or s2[i+2] == '#':
-#             return res
-#         i += 1
-#     return 0
+# use suffix array, connect the two string with some special char like '#'
+# the longest common substring is the longest duplicate substring of the new string
+# when we compare the longest common prefix of sorted suffix array, make sure there is '#' in one of the two str
+# that means that the two suffix substring come from different original string
+# time O((m+n)log(m+n)), space O(mn)
+def longest_common_substring_suffix_arr(s1, s2):
+    new_str = s1 + '#' + s2
+    suffix_arr = ['' for _ in range(len(new_str))]
+
+    for i in range(len(new_str)):
+        suffix_arr[i] = new_str[i:]
+    suffix_arr.sort()
+
+    max_len, res_substr = 0, set()
+    for i in range(len(new_str) - 1):
+        cur_len = compare_suffix_str(suffix_arr[i], suffix_arr[i+1])
+        max_len = max(max_len, cur_len)
+
+        if cur_len >= max_len:
+            res_substr.add(suffix_arr[i][:max_len])
+
+    for str in list(res_substr):
+        if len(str) < max_len:
+            res_substr.remove(str)
+    return max_len, res_substr
+
+
+def compare_suffix_str(s1, s2):
+    res = 0
+    if s1.find('#') == -1 and s2.find('#') == -1:
+        return res
+
+    for i in range(max(len(s1), len(s2))):
+        if s1[i] == s2[i]:
+            res += 1
+        else:
+            break
+    return res
 
 
 if __name__ == '__main__':
     s1 = 'I love programming all day and all night'
     s2 = 'If I have to write one more program, I will change majors'
 
-    res_dp = longest_common_substring_dp(s1, s2)
-    print res_dp[0], res_dp[1]
-
-    # print longest_common_substring_suffix_arr(s1, s2)
+    print longest_common_substring_dp(s1, s2)
+    print longest_common_substring_suffix_arr(s1, s2)
