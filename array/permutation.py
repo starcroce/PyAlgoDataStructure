b8b48208c7460d7428e4f657a8cf2f4ca350305f
yisha <yisha@securly.com>
2015-06-25 15:06:21 -0700

    add classic recursive solution for unique permutation

--- array/permutation.py
+++ array/permutation.py
@@ -21,6 +21,29 @@ def permutation_1_helper(nums, visited, sol, res):
             visited[i] = False
 
 
+def permutation_1_unique(nums):
+    res, sol = [], []
+    visited = [False for _ in range(len(nums))]
+    permutation_1_unique_helper(sorted(nums), visited, sol, res)
+    return res
+
+
+def permutation_1_unique_helper(nums, visited, sol, res):
+    if len(sol) == len(nums):
+        res.append(sol[:])
+        return
+
+    for i in range(len(nums)):
+        if visited[i] is False:
+            if i > 0 and visited[i-1] is False and nums[i] == nums[i-1]:
+                continue
+            visited[i] = True
+            sol.append(nums[i])
+            permutation_1_unique_helper(nums, visited, sol, res)
+            sol.pop()
+            visited[i] = False
+
+
 # [a] -> [ba, ab] -> [cba, bca, bac, cab, acb, abc]
 # replace the res list after inserting a new char
 def permutation_2(nums):
@@ -45,8 +68,10 @@ def permutation_2_unique(nums):
 
 
 if __name__ == '__main__':
-    nums = [1, 2, 3, 3]
+    nums = [2, 1, 2]
 
     print permutation_1(nums)
     print permutation_2(nums)
+
+    print permutation_1_unique(nums)
     print permutation_2_unique(nums)
